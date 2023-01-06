ed76f22397a9fafcf3ac3406b5493c93cc25b4d8
DRobertcpp <danila.robert148@gmail.com>
2023-01-06 17:09:39 +0200

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Sah
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aplicatieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iesireToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.intrumentulToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deseneazaTablaDeSahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajutorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.despreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.p)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // p
+            // 
+            this.p.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.p.Location = new System.Drawing.Point(46, 58);
+            this.p.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(537, 500);
+            this.p.TabIndex = 0;
+            this.p.TabStop = false;
+            this.p.Click += new System.EventHandler(this.p_Click);
+            this.p.Paint += new System.Windows.Forms.PaintEventHandler(this.p_Paint);
+            this.p.MouseDown += new System.Windows.Forms.MouseEventHandler(this.p_MouseDown);
+            this.p.MouseMove += new System.Windows.Forms.MouseEventHandler(this.p_MouseMove);
+            this.p.MouseUp += new System.Windows.Forms.MouseEventHandler(this.p_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aplicatieToolStripMenuItem,
+            this.intrumentulToolStripMenuItem,
+            this.ajutorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(604, 30);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aplicatieToolStripMenuItem
+            // 
+            this.aplicatieToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iesireToolStripMenuItem});
+            this.aplicatieToolStripMenuItem.Name = "aplicatieToolStripMenuItem";
+            this.aplicatieToolStripMenuItem.Size = new System.Drawing.Size(82, 24);
+            this.aplicatieToolStripMenuItem.Text = "Aplicatie";
+            // 
+            // iesireToolStripMenuItem
+            // 
+            this.iesireToolStripMenuItem.Name = "iesireToolStripMenuItem";
+            this.iesireToolStripMenuItem.Size = new System.Drawing.Size(127, 26);
+            this.iesireToolStripMenuItem.Text = "Iesire";
+            this.iesireToolStripMenuItem.Click += new System.EventHandler(this.iesireToolStripMenuItem_Click);
+            // 
+            // intrumentulToolStripMenuItem
+            // 
+            this.intrumentulToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deseneazaTablaDeSahToolStripMenuItem});
+            this.intrumentulToolStripMenuItem.Name = "intrumentulToolStripMenuItem";
+            this.intrumentulToolStripMenuItem.Size = new System.Drawing.Size(99, 26);
+            this.intrumentulToolStripMenuItem.Text = "Intrumentul";
+            // 
+            // deseneazaTablaDeSahToolStripMenuItem
+            // 
+            this.deseneazaTablaDeSahToolStripMenuItem.Name = "deseneazaTablaDeSahToolStripMenuItem";
+            this.deseneazaTablaDeSahToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.deseneazaTablaDeSahToolStripMenuItem.Text = "Joc nou";
+            this.deseneazaTablaDeSahToolStripMenuItem.Click += new System.EventHandler(this.deseneazaTablaDeSahToolStripMenuItem_Click);
+            // 
+            // ajutorToolStripMenuItem
+            // 
+            this.ajutorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.despreToolStripMenuItem});
+            this.ajutorToolStripMenuItem.Name = "ajutorToolStripMenuItem";
+            this.ajutorToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.ajutorToolStripMenuItem.Text = "Ajutor";
+            // 
+            // despreToolStripMenuItem
+            // 
+            this.despreToolStripMenuItem.Name = "despreToolStripMenuItem";
+            this.despreToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.despreToolStripMenuItem.Text = "Despre";
+            this.despreToolStripMenuItem.Click += new System.EventHandler(this.despreToolStripMenuItem_Click);
+            // 
+            // Sah
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 569);
+            this.Controls.Add(this.p);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Sah";
+            this.Text = "Sah";
+            this.Load += new System.EventHandler(this.Sah_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aplicatieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iesireToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem intrumentulToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deseneazaTablaDeSahToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajutorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem despreToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Sah : Form
+    {
+        Image img;
+        Graphics g;
+        JocSah joc;
+        int dx,dy;
+        bool apasat;
+        Piesa piesa;
+
+        public Sah()
+        {
+            InitializeComponent();
+        }
+
+        private void p_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iesireToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void despreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aplicatie realizata de Robert Dănilă");
+        }
+
+        private void deseneazaTablaDeSahToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            joc.jocNou();
+            p.Refresh();
+        }
+
+    
+
+        private void p_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+
+        private void Sah_Load(object sender, EventArgs e)
+        {
+            img = new Bitmap(p.Width, p.Height);
+            g = Graphics.FromImage(img);
+            joc = new JocSah(p.Width, p.Height, g);
+            apasat = false;
+
+        }
+
+        private void p_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (apasat)
+            {
+                piesa.X = e.X - dx;
+                piesa.Y = e.Y - dy;
+                joc.deseneaza();
+                p.Refresh();
+            
+            }
+        }
+
+        private void p_MouseUp(object sender, MouseEventArgs e)
+        { 
+            p.Cursor = Cursors.Default;
+            if (apasat)
+            {
+                joc.aseazaPiesa(piesa, e.X, e.Y);
+                joc.deseneaza();
+                p.Refresh();
+            }
+            apasat = false;
+        }
+
+
+        private void p_MouseDown(object sender, MouseEventArgs e)
+        {
+            piesa = joc.daPiesa(e.X, e.Y);
+            if (piesa != null)
+            {
+                apasat = true;
+                p.Cursor = Cursors.Hand;
+                dx = e.X - piesa.X;
+                dy = e.Y - piesa.Y;
+
+            }
+        }
+    }
+}
